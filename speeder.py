23aa0b61f5b1f4894d82d37494127fcf86e9af31
poissonnn <xxmoichehxx@gmail.com>
2026-04-01 15:05:36 +0200

    speeder finish

--- speeder.py
+++ speeder.py
@@ -23,6 +23,8 @@ import visual
 def radian(x):
     return 180 * x / np.pi
 
+# --- PYBULLET ---
+
 # p.GUI or p.DIRECT for non-graphical version
 physicsClient = p.connect(p.GUI)
 p.setGravity(0,0,-9.81)
@@ -35,13 +37,14 @@ planeId = p.loadURDF("plane.urdf" , [ 0 , 0 , 0 ] , [ 0 , 0 , 0 , 1 ] ) #charger
 
 sizeFactor = 20
 #spider = p.loadURDF(urdf_folder, [ 0 , 0 , 0 ] , [ 0 , 0 , 0 , 1 ] , globalScaling=sizeFactor , useFixedBase = True)
-spider = p.loadURDF(urdf_folder, [ 0 , 0 , 0 ] , [ 0 , 0 , 0 , 1 ] , globalScaling=sizeFactor)
+spider = p.loadURDF(urdf_folder, [ 0 , 0 , 0.14 ] , [ 0 , 0 , 0 , 1 ] , globalScaling=sizeFactor)
 obj_of_focus = spider
 
 # [0] number | [1] name | [2] type | [3] first position index | [4] first velocity index | [5] flags |
 # [6] damping | [7] friction | [8] lower limit | [9] upper limit | [10] max force | [11] max velocity |
 # [12] link name | [13] joint axis | [14] parent position | [15] parent orientation | [16] parent index
 
+"""
 for i in range(p.getNumJoints(spider)):
 
     name = p.getJointInfo(spider,i)[1]
@@ -55,74 +58,380 @@ for i in range(p.getNumJoints(spider)):
     print(f"{jlower} -> {jlowerDegree}")
     print(f"{jupper} -> {jupperDegree}")
     print()
-
-# robot | type of mouvement , target position
+"""
 
 position = 0.0
-stepMouvement = 0.05
+stepMouvement = 0.01
 
-joint_ids_base_legs =   [0, 3, 6, 9] # -90 // 90 | -1.5708 // 1.5708
-joint_ids_first_legs =  [1, 4, 7, 10] # -45 // 70 | -0.7853 // 1.2217
-joint_ids_second_legs = [2, 5, 8, 11] # -65 // 90 | -1.1344 // 1.5708
+joint_ids_base_legs =   [1, 4, 7, 10] # -90 // 90 | -1.5708 // 1.5708
+joint_ids_first_legs =  [2, 5, 8, 11] # -45 // 70 | -0.7853 // 1.2217
+joint_ids_second_legs = [3, 6, 9, 12] # -65 // 90 | -1.1344 // 1.5708
 
 clip_base_legs =   [-1.5708 , 1.5708]
 clip_first_legs =  [-0.7853 , 1.2217]
 clip_second_legs = [-1.1344 , 1.5708]
 
+"""
 print(clip_base_legs[0] , clip_base_legs [1])
 print(clip_first_legs[0] , clip_first_legs [1])
 print(clip_second_legs[0] , clip_second_legs [1])
-
-joint_ids = joint_ids_base_legs + joint_ids_first_legs + joint_ids_second_legs
-#joint_ids = [0,1,2,3,4,5,6,7,8,9,10,11,12]
-print(joint_ids)
+"""
+joint_ids = [0,1,2,3,4,5,6,7,8,9,10,11,12]
 
 len_joint_ids = len(joint_ids)
-print(len_joint_ids)
 target = [position] * len_joint_ids
-cliped = [position] * len_joint_ids 
+
+def reload():
+    for i in range(len_joint_ids):
+        target[i] = 0 # radian
+    
+    p.resetBasePositionAndOrientation(spider , [ 0 , 0 , 0.14 ] , [ 0 , 0 , 0 , 1 ])
+
+
+# --- PPO ---
+
+# ---[HYPERPARAMETERS]---
+lr = 3e-4
+max_grad_norm = 1.0
+
+clip_epsilon = (0.2) # value of the PPO loss
+gamma = 0.99
+lmbda = 0.95
+entropy_eps = 1e-3
+
+class ActorCritic(nn.Module):
+    def __init__(self, state_dim, action_dim, hidden_dim = 32):
+        super().__init__()
+
+        # X depends on the state_dim and actio_dim
+
+        # X input -> 32 hidden neurone -> 16 hidden neurone -> 16 hidden neurone
+        # this is a common part and divided to actor and to critic
+        self.shared_layers = nn.Sequential(
+            nn.Linear(state_dim, hidden_dim),
+            nn.ReLU(),
+            nn.Linear(hidden_dim, hidden_dim // 2 ),
+            nn.ReLU(),
+            nn.Linear(hidden_dim // 2, hidden_dim // 2 ),
+            nn.ReLU()
+        )
+
+        # 16 hidden neurone -> 8 hidden neurone -> X probability with the softmax
+        self.actor = nn.Sequential(
+            nn.Linear(hidden_dim // 2, hidden_dim // 4 ),
+            nn.ReLU(),
+            nn.Linear(hidden_dim // 4, action_dim),
+            nn.Softmax(dim=-1)
+        )
+
+        # 16 hidden neurone -> 8 hidden neurone -> 1 estimation of how many point the future ia can win
+        self.critic = nn.Sequential(
+            nn.Linear(hidden_dim // 2, hidden_dim // 4 ),
+            nn.ReLU(),
+            nn.Linear(hidden_dim // 4, 1)
+        )
+
+    def forward(self, x):
+        x = self.shared_layers(x)
+        action_probs = self.actor(x)
+        state_value = self.critic(x)
+        return action_probs, state_value
+
+# 0 state | 1 action | 2 reward | 3 next_state | 4 done | 5 state_value | 6 log_prob
+class buffer:
+    def __init__(self):
+        self.buffer = []
+    # store info | clear all | return
+    def store_buffer(self, state, action, reward, next_state, done, state_value, log_prob):
+        self.buffer.append(( state, action, reward, next_state, done, state_value, log_prob))
+
+    def clear_buffer(self):
+        self.buffer = []
+
+    def return_buffer(self):
+        return self.buffer
+
+    def sample(self, batch_size):
+        random.shuffle(self.buffer)
+
+        mini_batch = self.buffer[0:batch_size]
+
+        return mini_batch
+
+def observe():
+
+
+    state = [
+
+            ]
+
+
+    return np.array(state, dtype=np.float32)
+
+#take subdata and extract info 
+def extract_data_buffer(subdata, dataNumber):
+        data = []
+        for transcription in subdata:
+            data.append(transcription[dataNumber])
+
+        return data
+ 
+def compute_reward(done):
+
+    reward = 0
+
+    return reward, done
+
+def compute_returns(subdata):
+    G = 0
+    G_t = []
+    for transcription in reversed(subdata):
+        reward = transcription[2]
+        done = transcription[4]
+
+        G = reward + gamma * G * (1 - done)
+        G_t.insert(0,G)
+
+    return G_t
+
+def compute_advantages(subdata, values, dones, gamma, lambda_):
+    advantages = []
+    last_advantage = 0
+
+    rewards = extract_data_buffer(subdata, 2)
+    values = extract_data_buffer(subdata, 5)
+    values.append(0)
+    dones = extract_data_buffer(subdata, 4)
+
+    for t in reversed(range(len(rewards))):
+
+        mask = 1 - dones[t]
+        delta = rewards[t] + gamma * values[t + 1] * mask - values[t]
+        last_advantage = delta + gamma * lambda_ * mask * last_advantage
+        advantages.insert(0, last_advantage)
+    
+    return advantages
+
+    #rt+1​+γV(st+1​)−V(st​)
+    # reward + gamma * next state value - state_value
+
+def compute_value_loss(subdata, returns, model1):
+    states = extract_data_buffer(subdata, 0)
+    states = np.array(states)
+    states_tensor = torch.tensor(states, dtype=torch.float32)
+
+    returns_tensor = torch.tensor(returns, dtype=torch.float32).unsqueeze(-1)
+
+    action_probs, value_pred = model1(states_tensor)
+
+
+    loss_fn = nn.SmoothL1Loss()
+    value_loss = loss_fn(value_pred, returns_tensor).mean()
+
+    return value_loss
+
+def PPO_loss(subdata, advantages, model1):
+    states = extract_data_buffer(subdata, 0)
+    action = extract_data_buffer(subdata, 1)
+    old_log_prob = extract_data_buffer(subdata, 6)
+
+    states = np.array(states) # converting to np.array to be faster
+    action = np.array(action) # and removre the warning :D
+
+    action_tensor = torch.tensor(action, dtype=torch.int64)
+    state_tensor = torch.tensor(states, dtype=torch.float32)  # everything in tensor for the next math
+
+    action_probs, _ = model1(state_tensor)
+    action_dist = torch.distributions.Categorical(action_probs)
+
+    new_log_prob = action_dist.log_prob(action_tensor)
+    old_log_prob = torch.tensor(old_log_prob, dtype=torch.float32)
+
+    policy_ratio = torch.exp(new_log_prob - old_log_prob)
+
+    surrogate_loss_1 = policy_ratio * advantages.detach()
+
+    #print(surrogate_loss_1)
+
+    surrogate_loss_2 = torch.clamp(policy_ratio, 1-clip_epsilon, 1+clip_epsilon) * advantages
+
+    #print(surrogate_loss_2)
+
+    entropy = action_dist.entropy().mean()
+
+    entropy_bonus = entropy_eps * entropy
+
+    surrogate_loss = torch.min(surrogate_loss_1, surrogate_loss_2)
+    policy_loss = -torch.mean(surrogate_loss) - entropy_bonus
+    
+    return policy_loss
+
+def training(frames_per_batch, sub_batch_size, model1, max_training_frames):
+    D_buffer = buffer()
+
+    graphReward = []
+    episodeLength = []
+    optimizer = optim.Adam(model1.parameters(), lr=lr)
+
+    count_frame = 0
+    count_max_training_frames = 0
+
+    for i in range (frames_per_batch):    
+
+        keys = p.getKeyboardEvents()
+        if ord('s') in keys:
+            print('save')
+            break
+    
+        if ord('x') in keys:
+            reload()
+
+        # take info
+        Data = observe()          # data = state = s
+
+        if count_max_training_frames >= max_training_frames:
+
+            episodeLength.append(count_max_training_frames)
+
+            count_max_training_frames = 0
+            print(f"Over {max_training_frames} frames : reset")
+        
+        # put in tensor for the model
+        state_tensor = torch.tensor(Data, dtype=torch.float32)
+
+        # calculate the probability | state_value = value_pred
+        action_probs, state_value = model1(state_tensor)
+        state_value = state_value.detach().item()
+
+        # choose randomly one action while taking the probability
+        action_dist = torch.distributions.Categorical(action_probs)
+        action = action_dist.sample().item()        # action = a
+
+        # clip all the joint depending of their joint types
+        for i in range(len_joint_ids):
+
+            if i in joint_ids_base_legs:
+                target[i] = min(max(target[i], clip_base_legs[0]) , clip_base_legs[1])
+
+            elif i in joint_ids_first_legs:
+                target[i] = min(max(target[i], clip_first_legs[0]) , clip_first_legs[1])
+
+            elif i in joint_ids_second_legs:
+                target[i] = min(max(target[i], clip_second_legs[0]) , clip_second_legs[1])
+        # update the joint position
+        p.setJointMotorControlArray(spider,
+                                    joint_ids,
+                                    p.POSITION_CONTROL,
+                                    target)
+        
+        
+        focus_position , _ = p.getBasePositionAndOrientation(spider)
+            
+        p.resetDebugVisualizerCamera(cameraDistance=4,
+                                    cameraYaw=0,
+                                    cameraPitch=-40,
+                                    cameraTargetPosition = focus_position)
+
+        p.stepSimulation()
+        time.sleep(1./240.)
+        # --- after action ---
+
+        # use later for the ppo 
+        #(to know at which point the policy what thinking if it was right)
+        log_prob = action_dist.log_prob(torch.tensor(action, dtype=torch.int64)).item()
+
+        next_state = observe()      # next_state = s'
+
+        distance2 = compute_distance(next_state)
+        
+        reward, done = compute_reward()     # reward = r
+
+        graphReward.append(reward)
+
+        D_buffer.store_buffer(Data, action, reward, next_state, done, state_value, log_prob)
+
+        if done:
+            episodeLength.append(count_max_training_frames)
+            count_max_training_frames = 0
+            reload()
+            D_buffer.clear_buffer()
+            print(" - - DONE - - ")
+
+        # trainig loop
+        if len(D_buffer.buffer) >= sub_batch_size:
+
+            optimizer.zero_grad()
+
+            subdata = D_buffer.sample(sub_batch_size)
+
+            # calculate the amount of reward the model should get
+            # use to compare the model thus to update the model
+            returns = compute_returns(subdata)
+            
+            advantages = compute_advantages(subdata, model1,done, gamma, lmbda)
+            
+            advantages = torch.tensor(advantages, dtype=torch.float32)
+            advantages = (advantages - advantages.mean()) / (advantages.std() + 1e-8)
+
+
+            # calculate the loss of the policy for the actor
+            policy_loss =  PPO_loss(subdata, advantages, model1)
+
+            # calculate the loss of the policy for the critic
+            value_loss = compute_value_loss(subdata, returns, model1)
+
+            loss = policy_loss + value_loss 
+            #print(loss)
+            
+            loss.backward()
+
+            torch.nn.utils.clip_grad_norm_(model1.parameters(), max_grad_norm)
+
+            optimizer.step()
+
+            D_buffer.clear_buffer()
+
+        count_max_training_frames = count_max_training_frames +1
+        count_frame = count_frame + 1
+
+        if count_frame % max_training_frames*4 == 0:
+            print(f"frame count : {count_frame}")
+
+
+        #p.stepSimulation()
+        #time.sleep(1./240.)
+    visual.tensorGraphic(graphReward, episodeLength)
+
+path = r"/home/fish/FISH/prod/code/python/PyBullet/Drone"
+
+if not os.path.exists(path):
+    os.makedirs(path)
+
 
 while True:
     keys = p.getKeyboardEvents()
-
+    
     if ord('x') in keys:
-        target[1] += stepMouvement # radian
-        target[2] += stepMouvement # radian
+        reload()
 
-    
-    if ord('c') in keys:
-        target[1] -= stepMouvement # radian
-        target[2] -= stepMouvement # radian
-    
-    if ord('e') in keys:
-        target[3] += stepMouvement # radian
-        target[4] += stepMouvement # radian
-
-    if ord('d') in keys:
-        target[3] -= stepMouvement # radian
-        target[4] -= stepMouvement # radian
-    
+    # clip all the joint depending of their joint types
     for i in range(len_joint_ids):
 
         if i in joint_ids_base_legs:
-            x = min(max(target[i], clip_base_legs[0]) , clip_base_legs[1])
-            cliped[i] = x
+            target[i] = min(max(target[i], clip_base_legs[0]) , clip_base_legs[1])
 
         elif i in joint_ids_first_legs:
-            x = min(max(target[i], clip_first_legs[0]) , clip_first_legs[1])
-            cliped[i] = x
+            target[i] = min(max(target[i], clip_first_legs[0]) , clip_first_legs[1])
 
         elif i in joint_ids_second_legs:
-            x = min(max(target[i], clip_second_legs[0]) , clip_second_legs[1])
-            cliped[i] = x
-
-    #print(cliped)
+            target[i] = min(max(target[i], clip_second_legs[0]) , clip_second_legs[1])
+    # update the joint position
     p.setJointMotorControlArray(spider,
                                 joint_ids,
                                 p.POSITION_CONTROL,
                                 target)
     
-
+    
     focus_position , _ = p.getBasePositionAndOrientation(spider)
         
     p.resetDebugVisualizerCamera(cameraDistance=4,
