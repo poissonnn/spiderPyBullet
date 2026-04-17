c73c7603e348fe2c4c55b9d666649ef273d5074d
poissonnn <xxmoichehxx@gmail.com>
2026-04-17 20:06:29 +0200

    old data on graph

--- graphlab.py
+++ graphlab.py
@@ -1,60 +1,115 @@
 import matplotlib.pyplot as plt
 import numpy as np
-import random
+import pickle
 
-
-red_light   = [220/255, 100/255, 100/255]
 red_dark    = [97 /255, 0  /255, 6  /255]
-green_light = [100/255, 200/255, 100/255]
+red_light   = [220/255, 100/255, 100/255]
+orange_dark = [214/255, 40 /255, 40 /255]
+orange      = [255/255, 127/255, 17 /255]
+peach       = [255/255, 176/255, 144/255]
+yellow      = [252/255, 191/255, 73 /255]
+
 green_dark  = [80 /255, 180/255, 80 /255]
+green_light = [100/255, 200/255, 100/255]
+
+teal_dark   = [40 /255, 90 /255, 72 /255]
+teal_medium = [64 /255, 138/255, 113/255]
+teal_light  = [176/255, 228/255, 204/255]
+
+blue_dark   = [56 /255, 82 /255, 180/255]
 blue_light  = [100/255, 100/255, 220/255]
-orange      = [232/255, 127/255, 36 /255]
+
+purple      = [93 /255, 28 /255, 106/255]
+pink        = [202/255, 89 /255, 149/255]
+
 black       = [50 /255, 51 /255, 57 /255]
 true_black  = [0.1,0.1,0.1]
 gray        = [150/255, 150/255, 150/255]
 white       = [1, 1, 1]
 
-teal_light  = [176/255, 228/255, 204/255]
-teal_medium = [64 /255, 138/255, 113/255]
-teal_dark   = [40 /255, 90 /255, 72 /255]
-teal_black  = [9  /255, 20 /255, 19 /255]
+size = (14,12)
 
-Size = (20,20)
+graphPath = r"Graph/"
 
-fig, axs = plt.subplots(1,1,figsize=Size)
+def Reward(graphRewards):
+    print("reward graph")
+    
+    alpha = 1
+    zorder = 2
+    linewidth = 3.5
 
-SizeGraph = 12
-N = 1000
+    fig, ax = plt.subplots(figsize=size)
+    ax.set_title("Reward")
 
-size = 10
-border = 7
+    for graphRewards in allGraphRewards:
+        
+        alpha -= 0.20 
+        linewidth -= 0.5
+        zorder += 1
+        print(zorder)
 
+        ax.plot(graphRewards, color = black, zorder = 3, alpha = alpha,linewidth = linewidth)
 
+        #ax.fill_between(range(len(graphRewards)), np.array(graphRewards), where= np.array(graphRewards) > 0, color = yellow, alpha = alpha/3, interpolate = True,zorder = 1)
+        #ax.fill_between(range(len(graphRewards)), np.array(graphRewards), where= np.array(graphRewards) < 0, color = orange_dark  , alpha = alpha/3, interpolate = True, zorder = 1)
 
-x = []
-y = []
-number = 0
-while number < 5000:
-    #print("random")
-    temporaryX = random.uniform(-size, size)
-    temporaryY = random.uniform(-size, size)
+    ax.axhline(0, color = true_black, linestyle = (0,(2,2.5)), linewidth = 1.5, alpha = alpha)
+    ax.grid(which = "major", alpha = 0.25, linestyle = "--", linewidth = 0.8,color = gray, zorder = 0)
 
-    if temporaryY < -border or temporaryY > border or temporaryX < -border or temporaryX > border:
-        #print("graph")
-        x.append(temporaryX)
-        y.append(temporaryY)
-        number += 1
-
-
-
-
-axs.set_xlim(-SizeGraph, SizeGraph)
-axs.set_ylim(-SizeGraph, SizeGraph)
-
-axs.scatter(x ,y ,s = 50.0, color = red_light)
-
-axs.grid(which = "major", alpha = 0.25, linestyle = "--", linewidth = 0.8,color = gray, zorder = 0)
+    plt.tight_layout()
+    plt.savefig("Graph/Reward.png")
+    plt.close()
 
 
+linesCount = 0
 
-plt.show()
+with open("save.txt") as SaveFile:
+    
+    lines = SaveFile.readlines()
+
+counter = len(lines)
+#print(counter)
+
+
+
+# -- read all the data from the save file --
+num_epochsIndicies          = -1
+graphRewardsIndices         = -2
+episodeLengthIndices        = -3
+numpyGraphPolicyLossIndices = -4
+numpyGraphValueLossIndices  = -5
+graphEpisodeRewardIndices   = -6
+
+allGraphRewards = []
+
+batch = counter//6
+
+batchSize = counter//batch
+print(batchSize)
+print()
+
+for i in range(batch):
+    num_epochs = float(lines[num_epochsIndicies-batchSize*i].strip())
+
+    graphEpisodeReward = lines[graphRewardsIndices-batchSize*i].strip() 
+    graphEpisodeReward = [float(i) for i in graphEpisodeReward[1:-1].split(", ")] 
+
+
+    numpyGraphValueLoss = lines[episodeLengthIndices-batchSize*i].strip()
+    numpyGraphValueLoss = [float(i) for i in numpyGraphValueLoss[1:-1].split(", ")] 
+
+    numpyGraphPolicyLoss = lines[numpyGraphPolicyLossIndices-batchSize*i].strip()
+    numpyGraphPolicyLoss = [float(i) for i in numpyGraphPolicyLoss[1:-1].split(", ")]
+
+    episodeLength = lines[numpyGraphValueLossIndices-batchSize*i].strip()
+    episodeLength = [float(i) for i in episodeLength[1:-1].split(", ")] 
+
+    graphRewards = lines[graphEpisodeRewardIndices-batchSize*i].strip()
+    graphRewards = [float(i) for i in graphRewards[1:-1].split(", ")] # .split(", ") supp the useless part
+
+    allGraphRewards.append(graphRewards)
+    print("a")
+
+
+Reward(allGraphRewards)
+
